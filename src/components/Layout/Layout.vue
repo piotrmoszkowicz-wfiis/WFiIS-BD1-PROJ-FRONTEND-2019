d0ba30999e3fe1de916c2ac664ef9b5d355273d6
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-01-04 22:07:48 +0100

    Changes in Header, Layout and Sidebar

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,6 +1,5 @@
 <template>
-<div :class="{root: true, sidebarClose}">
-  <Helper />
+<div :class="{root: true}">
   <Header />
   <Sidebar />
   <div class="content animated fadeInUp">
@@ -9,33 +8,20 @@
     </transition>
   </div>
   <footer class="contentFooter">
-    Light Blue Vue Admin Dashboard Template - Made by <a href="https://flatlogic.com" target="_blank">Flatlogic</a>
+    Battlefield Play4Free Admin Panel - Made by <a href="https://github.com/piotrmoszkowicz" target="_blank">Piotr Moszkowicz</a>
   </footer>
 </div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
-
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Header from "@/components/Header/Header";
-import Helper from "@/components/Helper/Helper";
 
 import "./Layout.scss";
 
 export default {
   name: "Layout",
-  components: { Sidebar, Header, Helper },
-  methods: {
-    ...mapActions(
-      "layout", ["switchSidebar", "changeSidebarActive"],
-    ),
-  },
-  computed: {
-    ...mapState("layout", {
-      sidebarClose: state => state.sidebarClose,
-    }),
-  },
+  components: { Sidebar, Header },
   created() {
   },
 };
