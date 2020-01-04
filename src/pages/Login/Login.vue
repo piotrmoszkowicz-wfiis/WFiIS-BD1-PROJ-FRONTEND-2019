e4bc5eada880e5be166abd05c7e890052a0b1a63
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-01-04 20:59:53 +0100

    Translate & finish Login Page

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="auth-page">
     <b-container>
-      <Widget class="widget-auth mx-auto" title="<h3 class='mt-0'>Login to your Web App</h3>" customHeader>
+      <Widget class="widget-auth mx-auto" title="<h3 class='mt-0'>Battlefield Play4Free ACP</h3>" customHeader>
         <p class="widget-auth-info">
-            Use your email to sign in.
+            Użyj adresu email w celu zalogowania się.
         </p>
-        <form class="mt" @submit.prevent="login">
+        <form class="mt" @submit.prevent="handleLoginForm">
           <b-alert class="alert-sm" variant="danger" :show="!!errorMessage">
             {{errorMessage}}
           </b-alert>
@@ -20,7 +20,7 @@
                      placeholder="Email"/>
             </b-input-group>
           </b-form-group>
-          <b-form-group label="Password" label-for="password">
+          <b-form-group label="Hasło" label-for="password">
             <b-input-group class="input-group-no-border">
               <b-input-group-text slot="prepend"><i class="la la-lock text-white"/></b-input-group-text>
               <input id="password"
@@ -28,41 +28,29 @@
                      class="form-control input-transparent pl-3"
                      type="password"
                      required
-                     placeholder="Password"/>
+                     placeholder="Hasło"/>
             </b-input-group>
           </b-form-group>
           <div class="bg-widget auth-widget-footer">
-            <b-button type="submit" variant="danger" class="auth-btn" size="sm">
+            <b-button type="submit" variant="danger" class="auth-btn mb-3" size="sm">
               <span class="auth-btn-circle">
                 <i class="la la-caret-right"/>
               </span>
-              Login
+              Zaloguj
             </b-button>
-            <p class="widget-auth-info mt-4">
-              Don't have an account? Sign up now!
-            </p>
-            <router-link class="d-block text-center mb-4" to="login">Create an Account</router-link>
-            <div class="social-buttons">
-              <b-button variant="primary" class="social-button">
-                <i class="social-icon social-google"/>
-                <p class="social-text">GOOGLE</p>
-              </b-button>
-              <b-button variant="success" class="social-button">
-                <i class="social-icon social-microsoft"/>
-                <p class="social-text">MICROSOFT</p>
-              </b-button>
-            </div>
           </div>
         </form>
       </Widget>
     </b-container>
     <footer class="auth-footer">
-      Light Blue Vue Admin Dashboard Template - Made by <a href="https://flatlogic.com" target="_blank">Flatlogic</a>
+      Battlefield Play4Free Admin Panel - Made by <a href="https://github.com/piotrmoszkowicz" target="_blank">Piotr Moszkowicz</a>
     </footer>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
+
 import Widget from "@/components/Widget/Widget";
 
 export default {
@@ -73,19 +61,27 @@ export default {
       errorMessage: null,
     };
   },
+  computed: {
+    ...mapState("user", ["authToken"])
+  },
   methods: {
-    login() {
+    ...mapActions("user", ["logIn"]),
+    async handleLoginForm() {
       const email = this.$refs.email.value;
       const password = this.$refs.password.value;
 
       if (email.length !== 0 && password.length !== 0) {
-        window.localStorage.setItem("authenticated", true);
-        this.$router.push("/app/dashboard");
+        const loginResult = await this.logIn({ email, password });
+        if (loginResult) {
+          await this.$router.push("/app/dashboard");
+        } else {
+         this.errorMessage = "Błąd podczas logowania...";
+        }
       }
     },
   },
   created() {
-    if (window.localStorage.getItem("authenticated") === "true") {
+    if (this.authToken) {
       this.$router.push("/app/dashboard");
     }
   },
