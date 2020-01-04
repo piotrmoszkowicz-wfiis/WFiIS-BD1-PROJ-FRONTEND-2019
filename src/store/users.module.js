5c1f6a63f807f0609a3c1302f0887c2285db266a
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-01-04 22:44:16 +0100

    Add users store module

---
+++ src/store/users.module.js
@@ -0,0 +1,55 @@
+import { getUser, getUsers } from "../api";
+
+const initialState = () => ({
+  currentUser: null,
+  usersList: null
+});
+
+const state = initialState();
+
+const actions = {
+  async getUser({ commit }, userId) {
+    try {
+      const result = await getUser(userId);
+      if (result && result.data) {
+        commit("setUser", result.data);
+      }
+    } catch (err) {
+      // eslint-disable-next-line
+      console.error("Error while loading single user data", err);
+    }
+  },
+  async getUsers({ commit }) {
+    try {
+      const result = await getUsers();
+      if (result && result.data) {
+        commit("setUsers", result.data);
+      }
+    } catch (err) {
+      // eslint-disable-next-line
+      console.error("Error while loading users data...", err);
+    }
+  }
+};
+
+const mutations = {
+  setUsers(state, users) {
+    state.usersList = users;
+  },
+
+  setUser(state, user) {
+    state.currentUser = user;
+  },
+
+  updateUser(state, user) {
+    state.usersList = state.usersList.filter(existingUser => existingUser.id === user.id).push(user);
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations
+};
+
