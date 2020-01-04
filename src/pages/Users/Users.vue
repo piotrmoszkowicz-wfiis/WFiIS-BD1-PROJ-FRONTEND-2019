e433973ebb9f273cb81a2524164be7663bdaed83
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-01-04 22:44:25 +0100

    Add UsersList page

--- src/pages/Users/Users.vue
+++ src/pages/Users/Users.vue
@@ -1,20 +1,75 @@
 <template>
   <div>
     <b-breadcrumb>
-      <b-breadcrumb-item>YOU ARE HERE</b-breadcrumb-item>
-      <b-breadcrumb-item active>Typography</b-breadcrumb-item>
+      <b-breadcrumb-item>TU JESTEŚ</b-breadcrumb-item>
+      <b-breadcrumb-item active>Lista userów</b-breadcrumb-item>
     </b-breadcrumb>
-    <h1 class="page-title">
-      Another - <span class="fw-semi-bold">Page</span>
-    </h1>
+    <h2 class="page-title">
+      Lista <span class="fw-semi-bold">userów</span>
+    </h2>
+    <b-row>
+      <b-col>
+        <Widget>
+          <p>Znajdziesz tutaj listę wszystkich użytkowników w systemie.</p>
+          <div class="table-responsive">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Email</th>
+                  <th>Data utworzenia</th>
+                  <th>Prawa</th>
+                  <th>Akcje</th>
+                </tr>
+              </thead>
+              <tbody v-if="this.usersList">
+                <tr v-for="(user, index) in this.usersList" :key="`users_table_row_${index}`">
+                  <td>{{ user.id }}</td>
+                  <td>{{ user.email }}</td>
+                  <td>{{ user.createdAt }}</td>
+                  <td>{{ user.rights }}</td>
+                  <td>
+                    <router-link :to="`/app/users/edit/${user.id}`" class="text-info">
+                      <span class="icon">
+                        <i class="fi flaticon-edit"/>
+                      </span>
+                      Edytuj
+                    </router-link>
+                    <router-link :to="`/app/users/delete/${user.id}`" class="text-danger">
+                      <span class="icon">
+                        <i class="fi flaticon-trash"/>
+                      </span>
+                      Usuń
+                    </router-link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </Widget>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 import Widget from "@/components/Widget/Widget";
 
 export default {
   name: "Users",
   components: { Widget },
+  created() {
+    this.getUsers();
+  },
+  computed: {
+    ...mapState("users", ["usersList"])
+  },
+  methods: {
+    ...mapActions("users", ["getUsers"])
+  }
 };
 </script>
+
+<style src="./Users.scss" lang="scss" scoped />
+
