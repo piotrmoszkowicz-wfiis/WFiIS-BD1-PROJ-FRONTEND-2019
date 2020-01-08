54d10e4c2567e10d33886c2ace7a3e3f3e526233
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-01-08 21:08:50 +0100

    Remove not needed links

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -36,39 +36,6 @@
           index="items"
           isHeader
       />
-      <NavLink
-              header="Typography"
-              link="/app/typography"
-              iconName="flaticon-list"
-              index="typography"
-              isHeader
-      />
-      <NavLink
-              header="Tables Basic"
-              link="/app/tables"
-              iconName="flaticon-equal-1"
-              index="tables"
-              isHeader
-      />
-      <NavLink
-              header="Notifications"
-              link="/app/notifications"
-              iconName="flaticon-star"
-              index="notifications"
-              isHeader
-      />
-      <NavLink
-              :activeItem="activeItem"
-              header="Components"
-              link="/app/components"
-              iconName="flaticon-network"
-              index="components"
-              :childrenLinks="[
-          { header: 'Charts', link: '/app/components/charts' },
-          { header: 'Icons', link: '/app/components/icons' },
-          { header: 'Maps', link: '/app/components/maps' },
-        ]"
-      />
     </ul>
     <div class="sidebarAlerts d-sm-down-none">
       <b-alert
@@ -94,26 +61,6 @@ import NavLink from "./NavLink/NavLink";
 export default {
   name: "Sidebar",
   components: { NavLink },
-  data() {
-    return {
-      alerts: [
-        /* {
-          id: 0,
-          title: "Sales Report",
-          value: 15,
-          footer: "Calculating x-axis bias... 65%",
-          color: "info",
-        },
-        {
-          id: 1,
-          title: "Personal Responsibility",
-          value: 20,
-          footer: "Provide required notes",
-          color: "danger",
-        }, */
-      ],
-    };
-  },
   methods: {
     ...mapActions("layout", ["changeSidebarActive", "switchSidebar"]),
     setActiveByRoute() {
